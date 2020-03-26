7a0f701197a0f934b5fa9207b3dcdc29ac976c0c
Maciej Wnuk Lipiński <maciej.wnuklipinski@gmail.com>
2020-03-27 00:56:37 +0100

    changes in local.py

--- chariate_backend/settings/local.py
+++ chariate_backend/settings/local.py
@@ -44,7 +44,7 @@ SECRET_KEY = get_env_var('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
