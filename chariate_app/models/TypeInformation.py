566aa09f32658d5f9616c4c1f5763d980d45fdb5
Maciej Wnuk Lipiński <maciej.wnuklipinski@gmail.com>
2020-06-21 06:41:47 +0200

    TypeInformation - added max_use integer field

--- chariate_app/models/TypeInformation.py
+++ chariate_app/models/TypeInformation.py
@@ -7,6 +7,7 @@ class TypeInformation(models.Model):
         Model stores information data.
     '''
     name = models.CharField(max_length=100, null=False, unique=True)
+    max_use = models.IntegerField(null=True, default=1)
 
 
     class Meta:
